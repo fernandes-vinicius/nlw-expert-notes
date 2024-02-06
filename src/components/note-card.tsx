fb385f2c1ac3a5a20f223319cdcbd22cd7034881
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-02-06 17:51:40 -0300

    feat: add note details modal

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,17 +1,41 @@
-export function NoteCard() {
+import * as DialogPrimitive from '@radix-ui/react-dialog'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
+interface NoteCardProps {
+  note: {
+    date: Date
+    content: string
+  }
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-        deserunt architecto voluptate aliquid quis ipsam adipisci in, est eum
-        beatae, dolorum itaque ab natus ullam voluptates minima iste sunt
-        doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Odio iusto fugit ut sunt rerum mollitia eaque consectetur adipisci amet.
-        Itaque cupiditate nulla soluta deserunt adipisci esse mollitia modi,
-        consectetur dolorum!
-      </p>
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </button>
+    <DialogPrimitive.Root>
+      <DialogPrimitive.Trigger asChild>
+        <button className="rounded-md flex flex-col text-left bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+          <span className="text-sm font-medium text-slate-300">
+            {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+          </span>
+          <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+        </button>
+      </DialogPrimitive.Trigger>
+
+      <DialogPrimitive.Portal>
+        <DialogPrimitive.Overlay className="fixed inset-0 bg-black/50" />
+        <DialogPrimitive.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-[60vh] max-w-[640px] w-full bg-slate-700 rounded-md flex flex-col">
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className="text-sm font-medium text-slate-300">
+              {formatDistanceToNow(note.date, {
+                locale: ptBR,
+                addSuffix: true,
+              })}
+            </span>
+            <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+          </div>
+        </DialogPrimitive.Content>
+      </DialogPrimitive.Portal>
+    </DialogPrimitive.Root>
   )
 }
