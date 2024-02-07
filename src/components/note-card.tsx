1b23c74c80c616de722383d959c42f6693871794
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-02-07 10:34:06 -0300

    feat: add new note card form

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,7 @@
-import * as DialogPrimitive from '@radix-ui/react-dialog'
+import * as Dialog from '@radix-ui/react-dialog'
 import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
+import { XIcon } from 'lucide-react'
 
 interface NoteCardProps {
   note: {
@@ -11,8 +12,8 @@ interface NoteCardProps {
 
 export function NoteCard({ note }: NoteCardProps) {
   return (
-    <DialogPrimitive.Root>
-      <DialogPrimitive.Trigger asChild>
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
         <button className="rounded-md flex flex-col text-left bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
           <span className="text-sm font-medium text-slate-300">
             {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
@@ -20,11 +21,15 @@ export function NoteCard({ note }: NoteCardProps) {
           <p className="text-sm leading-6 text-slate-400">{note.content}</p>
           <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
         </button>
-      </DialogPrimitive.Trigger>
+      </Dialog.Trigger>
 
-      <DialogPrimitive.Portal>
-        <DialogPrimitive.Overlay className="fixed inset-0 bg-black/50" />
-        <DialogPrimitive.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-[60vh] max-w-[640px] w-full bg-slate-700 rounded-md flex flex-col">
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-black/50" />
+        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-full sm:h-[60vh] sm:max-w-[640px] w-full overflow-hidden bg-slate-700 rounded-md flex flex-col">
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <XIcon className="size-5" />
+          </Dialog.Close>
+
           <div className="flex flex-1 flex-col gap-3 p-5">
             <span className="text-sm font-medium text-slate-300">
               {formatDistanceToNow(note.date, {
@@ -34,8 +39,19 @@ export function NoteCard({ note }: NoteCardProps) {
             </span>
             <p className="text-sm leading-6 text-slate-400">{note.content}</p>
           </div>
-        </DialogPrimitive.Content>
-      </DialogPrimitive.Portal>
-    </DialogPrimitive.Root>
+
+          <button
+            type="button"
+            className="group w-full font-medium bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none"
+          >
+            Deseja{' '}
+            <span className="text-red-400 group-hover:underline">
+              apagar essa nota
+            </span>
+            ?
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
